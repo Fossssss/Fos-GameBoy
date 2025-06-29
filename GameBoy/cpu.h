cc4fbe87cce1ef6e7e6ed11308e2e9c5f4c42204
Fos <fresh_oranges@qq.com>
2025-06-29 16:42:53 +0800

    cpu

--- GameBoy/cpu.h
+++ GameBoy/cpu.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "common.h"
+#include "instructions.h"
 typedef struct {
 	u8 a;
 	u8 f;
@@ -16,9 +17,13 @@ typedef struct {
 	cpu_registers regs;
 	u16 fetch_data;
 	u16 mem_dest;
+	bool dest_is_mem;
 	u8 cur_opcode;
+	instruction* cur_inst;
 	bool halted;
-	bool steeping;
+	bool stepping;
+	bool int_master_enabled;
 }cpu_context;
 void cpu_init();
 bool cpu_setup();
+u16 cpu_read_reg(reg_type rt);
